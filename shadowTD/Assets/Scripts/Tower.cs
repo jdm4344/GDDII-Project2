bfb3c35483bfebbf8deb245389744f384e5d5ffc
Matt <mxr2091@g.rit.edu>
2018-10-09 17:22:05 -0400

    Gamemanager now sets up the enemy manger information Tower is able to update its in range enemies, identify a target, and track and attack that target

--- shadowTD/Assets/Scripts/Tower.cs
+++ shadowTD/Assets/Scripts/Tower.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour {
 
+    // Attributes
+
+    // where it is located
+    Vector3 position;
+    // which way it is facing, normalized
+    Vector3 direction;
+    // the current target
+    Enemy target;
+    // the amount of damage it does
+    protected int damage;
+    // cooldown between shots, in seconds
+    protected float cooldown;
+    // how much time has passed since the last shot
+    float timeSinceLastShot;
+    // how far it can shoot enemies at
+    protected float range;
+    // queue of in range enemies
+    List<Enemy> inRangeEnemies;
+
+    // access to the master list of enemies
+    EnemyManager enemyManager;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +33,71 @@ public class Tower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timeSinceLastShot += Time.deltaTime;
+
+        UpdateInRangeTargets();
+        UpdateTarget();
+        TrackTarget();
 	}
+
+    // updates the list of in range targets
+    void UpdateInRangeTargets() {
+        Enemy current;
+        // loop through all enemies on the field
+        for (int i = 0; i < enemyManager.enemyList.Capacity; i++)
+        {
+            current = enemyManager.enemyList[i];
+            // in range and not on the list
+            if ((current.position - this.position).magnitude <= range && !inRangeEnemies.Contains(current))
+            {
+                inRangeEnemies.Add(current);
+            }
+            // not in range and on the list
+            else if ((current.position - this.position).magnitude > range && inRangeEnemies.Contains(current))
+            {
+                inRangeEnemies.Remove(current);
+            }
+        }
+    }
+
+    // searches for the first vulnerable target in the list of in range targets, sets it to the current target
+    void UpdateTarget() {
+        for (int i = 0; i < inRangeEnemies.Capacity; i++)
+        {
+            if (inRangeEnemies[i].isVulnerable)
+            {
+                target = inRangeEnemies[i];
+                return;
+            }
+        }
+        // if there is no valid target
+        target = null;
+    }
+
+    // get the vector from this tower to the target, set to direction
+    void TrackTarget() {
+        if (target != null)
+        {
+            direction = (target.position - this.position).normalized;
+        }
+        else
+        {
+            Idle();
+        }
+    }
+
+    // deal damage to the target
+    void Attack() {
+        // check for cooldown
+        if (timeSinceLastShot >= cooldown)
+        {
+            timeSinceLastShot = 0.0f;
+            target.TakeDamage(damage);
+        }
+    }
+
+    // where to look if it isn't tracking a target
+    void Idle() {
+        direction = Vector3.up;
+    }
 }
